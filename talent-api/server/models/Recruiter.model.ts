54a3d855954cf814cc829d2c45e55843e05ffbf3
Leonardo <cifuentes.k.l@gmail.com>
2020-11-06 17:55:11 -0300

    feat/recruiter_model_creation

---
+++ talent-api/server/models/Recruiter.model.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  class Recruiter extends Model {
+    static associate(models) {
+      // define associations here, e.g.
+      this.belongsToMany(models.Candidate, {
+        through: 'folder_candidates',
+      });
+    }
+  }
+
+  Recruiter.init(
+    {
+      contactName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false}, 
+      company: { type: DataTypes.STRING, allowNull: false},
+      siteUrl: { type: DataTypes.STRING, allowNull: false},
+    },
+    {
+      sequelize,
+      modelName: 'recruiter',
+    }
+  );
+
+  return Recruiter;
+};
