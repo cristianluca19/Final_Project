439bba72bc4c86219a29a66558b372acc418c3ef
unknown <sebalevin@hotmail.com.ar>
2020-11-07 19:42:14 -0300

    fixed relationship between skills and candidates

---
+++ talent-api/server/models/Skill.model.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import sequelize from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+    class Skill extends Model {
+        static associate(models) {
+            // define associations here, e.g.
+            this.belongsToMany(models.Candidate, {
+                through: 'candidate_skills',
+            });
+        }
+    }
+
+
+    Skill.init(
+        {
+            name: { type: DataTypes.STRING, allowNull: false },
+            type: { type: DataTypes.ENUM('hard', 'soft') }
+        },
+        {
+            sequelize,
+            modelName: 'skills'
+        }
+    )
+    return Skill;
+}
