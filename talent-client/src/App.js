4850fc3e9e450fc9f1f51542ff6020ee65485bd1
Diego Emanuel Tolaba <diegotolaba09@gmail.com>
2020-11-10 12:10:45 -0300

    feat(client): landing-page

--- talent-client/src/App.js
+++ talent-client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Route } from "react-router-dom";
 import './App.css';
+import Catalogue from "./components/Catalogue/index.jsx";
+import ContentHome from "./components/ContentHome/index.jsx";
+import Footer from "./components/Footer/index.jsx";
+import Nav from "./components/Nav/index.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/' render={() => <Nav />} />
+      <Route path='/' render={() => <ContentHome />} />
+      <Route path='/' render={() => <Catalogue />} />
+      <Route path='/' render={() => <Footer />} />
     </div>
   );
 }
---
+++ talent-client/src/components/Nav/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import { Grid, Container, makeStyles, IconButton } from '@material-ui/core';
+import FolderIcon from '@material-ui/icons/Folder';
+import AcountCircle from '@material-ui/icons/AccountCircle';
+import Settings from '@material-ui/icons/Settings';
+import Menu from './menu.jsx';
+
+const useStyle = makeStyles({
+    logo: {
+        padding: '50px 0px'
+    },
+    icons: {
+        color: '#ffff00',
+        maxWidth: '120px'
+    },
+    containerIcons: {
+        justifyContent: 'flex-end'
+    }
+})
+
+function Nav() {
+
+    const classes = useStyle();
+
+    return (
+        <nav>
+            <Container maxWidth="lg">
+                <Grid container spacing={1}>
+                    <Grid container item xs={3} sm={3} spacing={3}>
+                        <img className={classes.logo} src={logo} />
+                    </Grid>
+                    <Grid container item xs={6} sm={6} spacing={3}>
+                        <Menu />
+                    </Grid>
+                    <Grid className={classes.containerIcons} container item xs={3} sm={3} spacing={1}>
+                        <IconButton className={classes.icons} label="Acount Circle" value="folder">
+                            <AcountCircle />
+                        </IconButton>
+                        <IconButton className={classes.icons} label="Folder" value="folder" >
+                            <FolderIcon />
+                        </IconButton>
+                        <IconButton className={classes.icons} label="Setting" value="folder">
+                            <Settings />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Container>
+        </nav>
+    );
+};
+
+export default Nav;
