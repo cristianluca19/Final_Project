516246e8fe8bfb6c992e439e5da93ddc2120d6a3
Martin Cura <martincura@gmail.com>
2020-11-11 21:21:45 -0300

    fix(api/ci): remove mocha timeout, sync then run()

--- talent-api/test/resetDB.ts
+++ talent-api/test/resetDB.ts
@@ -5,11 +5,5 @@ import db from '../server/models';
 setTimeout(() => run(), process.env.CI ? 10000 : 1000);
 
 before('Syncing and resetting database', async () =>
-  db.sequelize.sync({ force: true })
+  db.sequelize.sync({ force: true }).then(() => run())
 );
-
-// export const mochaHooks = {
-//   async beforeAll(): Promise<void> {
-//     await db.sequelize.sync({ force: true });
-//   },
-// };
