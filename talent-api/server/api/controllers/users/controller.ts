c7129c710d7d83714ee09e06b3cf75121b3a2424
FedericoCalderon <fede4872@gmail.com>
2020-11-13 14:37:40 -0300

    refactor/create_user

--- talent-api/server/api/controllers/users/controller.ts
+++ talent-api/server/api/controllers/users/controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import db from '../../../models';
 
 export class usersController {
-
   async createUser(req: Request, res: Response): Promise<void> {
     const { firstName, lastName, profilePicture, role } = req.body;
     const user = await db.User.create({
