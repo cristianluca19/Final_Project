c9ff308e267fd2a93a73c8e465932b65015eeb43
FedericoCalderon <fede4872@gmail.com>
2020-11-17 18:02:13 -0300

    fix/change format from body to query in candidates_filter

--- talent-api/server/api/controllers/filter/controller.ts
+++ talent-api/server/api/controllers/filter/controller.ts
@@ -4,7 +4,18 @@ import db from '../../../models';
 
 export class candidatesFilter {
   async filter(req: Request, res: Response): Promise<void> {
-    const { skillsArray, cohortArray, locationArray } = req.body;
+    // const { skillsArray, cohortArray, locationArray } = req.body;
+    const skills = req.query.skills || '';
+    const cohorts = req.query.cohorts || '';
+    const location = req.query.locations || '';
+    let skillsArray, cohortArray, locationArray;
+    skills ? (skillsArray = skills.toString().split(',')) : (skillsArray = []);
+    cohorts
+      ? (cohortArray = cohorts.toString().split(','))
+      : (cohortArray = []);
+    location
+      ? (locationArray = location.toString().split(','))
+      : (locationArray = []);
     const consult = {
       where: {
         cohort: cohortArray,
@@ -21,17 +32,16 @@ export class candidatesFilter {
       if (!skillsArray.length) delete consult.include;
       if (!cohortArray.length) delete consult.where.cohort;
       if (!locationArray.length) delete consult.where.country;
-      const candidatesFiltered = await db.Candidate.findAll(consult);
-      if (!candidatesFiltered.length){
-        res
-          .send(204)
-          .send('No se encontraron candidatos que cumplan con lo especificado');
+      if (!skillsArray.length && !cohortArray.length && !locationArray.length) {
+        res.sendStatus(204);
         return;
       }
+
+      const candidatesFiltered = await db.Candidate.findAll(consult);
       res.status(200).json(candidatesFiltered);
       return;
     } catch (err) {
-      res.status(400).json(err);
+      res.sendStatus(400);
       return;
     }
   }
