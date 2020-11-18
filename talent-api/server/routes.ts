80fbe01efe1bf5aa5933a7982bb9d095104407b9
FedericoCalderon <fede4872@gmail.com>
2020-11-17 22:03:57 -0300

    feat error in candidate test

--- talent-api/server/routes.ts
+++ talent-api/server/routes.ts
@@ -4,7 +4,7 @@ import candidatesRouter from './api/controllers/candidates/router';
 import recruitersRouter from './api/controllers/recruiters/router';
 import foldersRouter from './api/controllers/folders/router';
 import usersRouter from './api/controllers/users/router';
-import candidateFilter from './api/controllers/filter/router'
+import candidateFilter from './api/controllers/filter/router';
 
 export default function routes(app: Application): void {
   app.use('/api/v1/candidates', candidatesRouter);
