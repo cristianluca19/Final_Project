e261e915bb5483febeeadcff3086761ab0bcdbff
FedericoCalderon <fede4872@gmail.com>
2020-11-18 20:31:54 -0300

    feat/seeders skills_candidates

--- talent-api/db/seeders/20201111135422-skills.js
+++ talent-api/db/seeders/20201111135422-skills.js
@@ -2,7 +2,24 @@
 const faker = require('faker');
 
 const skills = {
-  hard: ['javascript', 'react', 'html5', 'nodeJs', 'express', 'sequelize'],
+  hard: [
+    'javascript',
+    'react',
+    'html5',
+    'nodeJs',
+    'express',
+    'sequelize',
+    'php',
+    'python',
+    'flask',
+    'passport',
+    'java',
+    'C',
+    'AWS',
+    'C++',
+    'angular',
+    'C#',
+  ],
   soft: ['compañerismo', 'trabajo en equipo', 'liderazgo', 'flexible'],
 };
 
@@ -10,22 +27,50 @@ function mixer(skills) {
   let skillType = ['hard', 'soft'];
   let randomSkill = skillType[Math.floor(Math.random() * 2)];
   let mix = {
-    name: skills[randomSkill][Math.floor(Math.random() * (randomSkill.length - 1))],
+    name:
+      skills[randomSkill][Math.floor(Math.random() * (randomSkill.length - 1))],
     type: randomSkill,
     created_at: faker.date.past(),
     updated_at: faker.date.recent(),
   };
   return mix;
 }
+function relationCandidateSkills(skillsTable) {
+  let candidate_skills = [];
+  let count = 0;
+  for (let i = 1; i < skillsTable.length; i++) {
+    if (i % 5 === 0) {
+      count = i;
+    }
+    for (let j = 1; j <= 5; j++) {
+      let skill = {
+        created_at: faker.date.past(),
+        updated_at: faker.date.recent(),
+        skill_id: j + count,
+        candidate_id: i,
+      };
+      candidate_skills.push(skill);
+    }
+  }
+  return candidate_skills;
+}
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     let count = 20;
     let skillsRandom = [];
+    let mixInsert;
     while (count--) {
-      skillsRandom.push(mixer(skills));
+      mixInsert = mixer(skills);
+      if (!skillsRandom.includes(mixInsert)) {
+        skillsRandom.push(mixInsert);
+      }
     }
     await queryInterface.bulkInsert('skills', skillsRandom, {});
+    await queryInterface.bulkInsert(
+      'candidate_skills',
+      relationCandidateSkills(skillsRandom)
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
