b469e519f3f97870555b1374d99a3804d924c0ea
FedericoCalderon <fede4872@gmail.com>
2020-11-19 11:06:58 -0300

    feat/add folder_candidates seeds

--- talent-api/db/seeders/20201119004317-folders.js
+++ talent-api/db/seeders/20201119004317-folders.js
@@ -2,7 +2,7 @@
 const faker = require('faker');
 const uuid = require('uuidv4');
 function fill() {
-  let folders = [];
+  const folders = [];
   const status = ['created', 'sent'];
   let id = 1;
   for (let folder = 14; folder >= 1; folder--) {
@@ -21,9 +21,49 @@ function fill() {
   return folders;
 }
 
+// function relationCandidatesFolders() {
+//   let random;
+//   for (let i = 1; i < array.length; i++) {
+//     random = faker.number.random(14);
+//     folder_candidates.push({
+//       folder_id: random,
+//       candidate_id: random,
+//     });
+//   }
+//   // for (let j = 0; j < array.length; j++) {
+//   // }
+//   // if (!folder_candidates.includes(random) && i !==1){
+//   // }
+//   return folder_candidates;
+// }
+function relationCandidatesFolders() {
+  const folder_candidates = [];
+  let count = 0;
+  for (let i = 1; i < 15; i++) {
+    if (i % 5 === 0) {
+      count = i;
+    }
+    for (let j = 1; j <= 5; j++) {
+      let candidate = {
+        created_at: faker.date.past(),
+        updated_at: faker.date.recent(),
+        candidate_id: j + count,
+        folder_id: i,
+      };
+      folder_candidates.push(candidate);
+    }
+  }
+  return folder_candidates;
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('folders', fill(), {});
+    await queryInterface.bulkInsert(
+      'folder_candidates',
+      relationCandidatesFolders(),
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
