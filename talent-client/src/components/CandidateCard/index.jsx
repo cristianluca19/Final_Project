44145ca9094a98577e04368c994b13ebb6b042ba
lukasver <l.verdiell@gmail.com>
2020-11-25 11:30:56 -0300

    style/deleted N/A for -

--- talent-client/src/components/CandidateCard/index.jsx
+++ talent-client/src/components/CandidateCard/index.jsx
@@ -251,7 +251,7 @@ function CandidateCard(props) {
                     <Chip
                       className={classes.scoring}
                       size="small"
-                      label={candidate.score || 'N/A'}
+                      label={candidate.score || '-'}
                       icon={<GradeIcon />}
                     />
                   </Badge>
