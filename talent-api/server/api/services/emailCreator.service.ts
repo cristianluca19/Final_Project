5d911c9a328b2d26dbcae7b23138344f7e802a34
FedericoCalderon <fede4872@gmail.com>
2020-11-26 13:12:46 -0300

    refactor/recruiter_mail

--- talent-api/server/api/services/emailCreator.service.ts
+++ talent-api/server/api/services/emailCreator.service.ts
@@ -1,46 +1,28 @@
-import mail from 'mailgun-js';
 import '../../common/env';
-let mailGun;
-let mailCreator;
+import mail from 'mailgun-js';
+import mailTemplate from '../templates/mail.template';
+let mailGunConfig;
+let sendMail;
 if (
   process.env.MAILGUN_APIKEY &&
   process.env.MAILGUN_DOMAIN &&
   process.env.LINK
 ) {
-  mailGun = mail({
+  mailGunConfig = mail({
     apiKey: process.env.MAILGUN_APIKEY,
     domain: process.env.MAILGUN_DOMAIN,
   });
 
-  mailCreator = function mailGenerator(to, uuid) {
+  sendMail = function mailSender(to, uuid) {
     const mailOptions = {
       from: 'talenthenry2020@gmail.com',
       to: to,
       subject: 'Talent Support',
-      html: `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body>
-          <div>
-              <h3>Folder with candidates</h3>
-              <a href=${process.env.LINK}${uuid}> Open folder here </a>
-              <p>thanks for doing business with us</p>
-          </div>
-      </body>
-      </html>`,
+      html: mailTemplate(process.env.LINK, uuid),
     };
-    mailGun.messages().send(mailOptions, (err, data) => {
+    mailGunConfig.messages().send(mailOptions, (err, data) => {
       if (err) console.log('Error => ', err);
-      else {
-        console.log(`
-      Email sent sucsessfully to ${to}!!
-      More details:`);
-        console.log(data);
-      }
     });
   };
 }
-export default mailCreator;
+export default sendMail;
