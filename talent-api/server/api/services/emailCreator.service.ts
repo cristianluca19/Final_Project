12f237f8d34d0dfdeb13912e17b6a9b0fa0bbdf9
FedericoCalderon <fede4872@gmail.com>
2020-11-26 11:28:22 -0300

    fix/recruiter_mail emailCreator.service solved

--- talent-api/server/api/services/emailCreator.service.ts
+++ talent-api/server/api/services/emailCreator.service.ts
@@ -1,31 +1,37 @@
 import mail from 'mailgun-js';
 import '../../common/env';
-const mailGun = mail({
-  apiKey: process.env.MAILGUN_APIKEY,
-  domain: process.env.MAILGUN_DOMAIN,
-});
+let mailGun;
+let mailCreator;
+if (
+  process.env.MAILGUN_APIKEY &&
+  process.env.MAILGUN_DOMAIN &&
+  process.env.LINK
+) {
+  mailGun = mail({
+    apiKey: process.env.MAILGUN_APIKEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
 
-function mailCreator(to, uuid) {
-  const mailOptions = {
-    from: 'talenthenry2020@gmail.com',
-    to: to,
-    subject: 'Talent Support',
-    html: `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>
-            <h3>Folder with candidates</h3>
-            <a href=${process.env.LINK}${uuid}> Open folder here </a>
-            <p>thanks for doing business with us</p>
-        </div>
-    </body>
-    </html>`,
-  };
-  if (process.env.NODE_ENV !== 'ci') {
+  mailCreator = function mailGenerator(to, uuid) {
+    const mailOptions = {
+      from: 'talenthenry2020@gmail.com',
+      to: to,
+      subject: 'Talent Support',
+      html: `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </head>
+      <body>
+          <div>
+              <h3>Folder with candidates</h3>
+              <a href=${process.env.LINK}${uuid}> Open folder here </a>
+              <p>thanks for doing business with us</p>
+          </div>
+      </body>
+      </html>`,
+    };
     mailGun.messages().send(mailOptions, (err, data) => {
       if (err) console.log('Error => ', err);
       else {
@@ -35,6 +41,6 @@ function mailCreator(to, uuid) {
         console.log(data);
       }
     });
-  }
+  };
 }
 export default mailCreator;
